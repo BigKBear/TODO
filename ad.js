94d2719e23dd345da3192db2a7bd9f00fcbf508d
BigKBear <kylesthill@hotmail.com>
2015-08-12 14:25:08 -0400

    Get entered text and show in list

--- ad.js
+++ ad.js
@@ -1,21 +1,21 @@
+/*
+ model
+ */
+var list_item = { name:" ", selected:false};
+
+/*
+ controler
+ */
 function add_to_list()
 {
-    var task = $("#get_entered_text").val();
+    //var task = $("#get_entered_text").val();
+    var task = document.getElementById("get_entered_text").value;
     
     $("#dunamicaly_added_items").append("<div>"+task+" <button type='button'> Edit </button><button type='button'> Delete </button> </div><br>")
     
     $("#get_entered_text").val("");
 }
 
-
-
-/*
- model
- */
-
-/*
- controler
- */
 
 /*
  View
