1d314eeb8eadffdaeda2750be08d1ee951a256a5
BigKBear <kylesthill@hotmail.com>
2015-08-12 15:47:49 -0400

    Done Javascript

--- ad.js
+++ ad.js
@@ -8,14 +8,12 @@ var list_item = { name:" ", selected:false};
  */
 function add_to_list()
 {
-    //var task = $("#get_entered_text").val();
     var task = document.getElementById("get-entered-text").value;
     
     var rowitem = document.createElement("li");
     rowitem.appendChild(document.createTextNode(task));
     document.getElementById("dunamicaly-added-items").appendChild(rowitem);
     
-    //$("#dunamicaly-added-items").append("<li> <input type='checkbox'>"+task+" <button type='button'> Edit </button><button type='button'> Delete </button> </input></li><br>")
     
 }
 
