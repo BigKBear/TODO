3e7bafd2ee80f603a665997d5dc08c05d30657d5
BigKBear <kylesthill@hotmail.com>
2015-08-12 18:57:51 -0400

    Working clone but not efficent code

--- ad.js
+++ ad.js
@@ -1,21 +1,33 @@
 /*
+ Challange
+ */
+
+/*
  model
  */
-var list_item = { name:" ", selected:false};
+    var listItem = { name:" ", selected:false};
 
 /*
  controler
  */
-function add_to_list()
-{
-    var task = document.getElementById("get-entered-text").value;
+    function addToList()
+    {
+        var task = document.getElementById("get-entered-text").value;
+        task.onchange
+        var rowitem = document.createElement("li");
+        rowitem.appendChild(document.createTextNode(task));
     
-    var rowitem = document.createElement("li");
-    rowitem.appendChild(document.createTextNode(task));
-    document.getElementById("dunamicaly-added-items").appendChild(rowitem);
-    
-    
-}
+        document.getElementById("dunamicaly-added-items").appendChild(rowitem);
+    }
+
+    function Mimic()
+    {
+        var task = document.getElementById("get-entered-text").value;
+        var task2 = document.getElementById("get-entered-text2").value = task;
+                var task2 = document.getElementById("get-entered-text3").value = task;
+                var task2 = document.getElementById("get-entered-text4").value = task;
+    }
+
 
 
 /*
