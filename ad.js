4a2288ca06a646b67debbf22a3446d50afc03984
BigKBear <kylesthill@hotmail.com>
2015-08-19 07:16:27 -0400

    Basic set up to start angular app

--- ad.js
+++ ad.js
@@ -23,75 +23,24 @@ Read the source code!
  - Consumable
  - Error messages are bad!
  - Dissect the source
-
-
- 
 */
+
+angular.module('Todo-app', []);
+
 
 /*
  Challange
  */
 
-/*
- if(el){
- el.addEventListener('click', swapper, false);
- }
- */
-//document.getElementById("add-to-list").addEventListener("click", addToList);
 
 
 /*
  model
-
- Person = function(name, age) {
- this.watch('age', Person.prototype._isValidAssignment);
- this.watch('name', Person.prototype._isValidAssignment);
- this.name = name;
- this.age = age;
- };
  */
-var listItem = { name:"", selected:false};
 
 
 /*
  controler
- */
-function addToList()
-{
-    var task = document.getElementById("get-entered-text").value;
-    task.onchange
-    var rowitem = document.createElement("li");
-    rowitem.appendChild(document.createTextNode(task));
-    
-    document.getElementById("dunamicaly-added-items").appendChild(rowitem);
-}
-
-
-function changeMade(r)
-{
-    console.log(r.target.value);
-//    console.log('Value in r which is pased to function checkbox is '+ r.target.checked + ' the list item name that the user entered is ' + r.target.value)
-//    return r.target.value ;
-}
-
-var mimic = function(e) {
-    listItem.name = e.target.value;
-
-}
-
-/*
- o.watch('p', function (id, oldval, newval) {
- console.log('o.' + id + ' changed from ' + oldval + ' to ' + newval);
- return newval;
- });
- 
- o.p = 2;
- o.p = 3;
- delete o.p;
- o.p = 4;
- 
- o.unwatch('p');
- o.p = 5;
  */
 
 
@@ -100,14 +49,8 @@ var mimic = function(e) {
  View
  */
 
+
+
 /*
  Templater
  */
-
-var same = document.getElementsByClassName("same");
-
-for(var i=0; i<same.length; i++){
-    same[i].addEventListener("keyup", mimic);
-}
-
-listItem.observe('name', changeMade);
