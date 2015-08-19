dbe43b796e00fb2059c0b4489224209c46fd19d2
BigKBear <kylesthill@hotmail.com>
2015-08-19 07:06:09 -0400

    Started with notes on best pratices

--- ad.js
+++ ad.js
@@ -1,3 +1,33 @@
+
+/*
+Best Pratices
+ - Model-View* Separation
+ - DRY programming (do not repeat yourself)
+ - Dont hack (find a way to stick to th frame work)
+
+Dont write bad code
+ - Performance
+ - Reusability
+ - Square peg, round hole 
+ 
+Test
+ - Write the test!
+ - TDD with a robust test suite
+
+Broaden your understanding
+ - Learning curve is steep!
+ - Pratice being curious
+ - Refactoring (teaches about cleaner code)
+
+Read the source code!
+ - Consumable
+ - Error messages are bad!
+ - Dissect the source
+
+
+ 
+*/
+
 /*
  Challange
  */
@@ -12,8 +42,16 @@
 
 /*
  model
+
+ Person = function(name, age) {
+ this.watch('age', Person.prototype._isValidAssignment);
+ this.watch('name', Person.prototype._isValidAssignment);
+ this.name = name;
+ this.age = age;
+ };
  */
 var listItem = { name:"", selected:false};
+
 
 /*
  controler
@@ -29,9 +67,33 @@ function addToList()
 }
 
 
+function changeMade(r)
+{
+    console.log(r.target.value);
+//    console.log('Value in r which is pased to function checkbox is '+ r.target.checked + ' the list item name that the user entered is ' + r.target.value)
+//    return r.target.value ;
+}
+
 var mimic = function(e) {
     listItem.name = e.target.value;
+
 }
+
+/*
+ o.watch('p', function (id, oldval, newval) {
+ console.log('o.' + id + ' changed from ' + oldval + ' to ' + newval);
+ return newval;
+ });
+ 
+ o.p = 2;
+ o.p = 3;
+ delete o.p;
+ o.p = 4;
+ 
+ o.unwatch('p');
+ o.p = 5;
+ */
+
 
 
 /*
@@ -47,3 +109,5 @@ var same = document.getElementsByClassName("same");
 for(var i=0; i<same.length; i++){
     same[i].addEventListener("keyup", mimic);
 }
+
+listItem.observe('name', changeMade);
