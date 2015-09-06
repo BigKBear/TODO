a232aadf6caabbf7937ce106019091a4e8518945
BigKBear <kylesthill@hotmail.com>
2015-09-06 14:17:02 -0400

    the start of the JS file

---
+++ getalltodos.js
@@ -0,0 +1,17 @@
+var todoapp = angular.module('todoapp',[]);
+
+todoapp.factory('Todos',function(){
+	var todos ={};
+	todos.list = [
+	{name: 'Wash Dishes', isDone:'false'},
+	{name: ' Put clothes in Machine', isDone:'false'},
+	{name: 'Cook Food', isDone:'false'},
+	{name: 'Clean Bath', isDone:'false'},
+	{name: 'Hangout Clothes', isDone:'false'}
+];
+	return todos;
+})
+
+function TodosCtrl($scope, Todos){
+	$scope.todos = Todos;
+}
