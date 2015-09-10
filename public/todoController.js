bc134d8234f9e7f46c5335746b2b3d675fb7aa4a
BigKBear <kylesthill77@gmail.com>
2015-09-10 19:34:08 -0400

    pull from server using http request

---
+++ public/todoController.js
@@ -0,0 +1,62 @@
+/*
+angular.module("todoapp").controller('TodoController', function($scope, TodoService){
+	$scope.todos = [];
+	
+	TodoService.getAllTodos().then(function (data) {
+		$scope.todos = data.data;
+		console.log($scope.todos);
+		console.log(data.data);
+	});//we should not now be getting the data the data should already been here "route resolve"
+
+	$scope.addTodo= function(){
+		if(!$scope.addtodo) {
+			console.log("you did not enter a todo");
+			return false;
+		} 
+
+		$scope.todos.push({
+			name:$scope.addtodo,
+			isDone:false
+		});
+		$scope.addtodo="";
+	};
+
+	$scope.deleteTodo= function(index){
+		$scope.todos.splice($scope.todos.indexOf(index));	
+		console.log("deleted");
+	};
+
+	// Get Total Items
+	$scope.getTotalTodos= function () {
+	    return $scope.todos.length;
+	};
+
+	// Get Total Checked Items
+	$scope.getTotalCheckedTodos = function () {
+		var doneNumber = 0;
+	    for (var i = 0; i < $scope.todos.length; i++) {
+	    	if ($scope.todos[i].isDone) {
+	    		doneNumber++;
+	    	}
+	    };
+	    return doneNumber;
+	};
+
+	// Change false to true
+	$scope.toggleChecked = function (todo) {
+		console.log(todo.isDone);
+	    todo.isDone = !todo.isDone;
+	};
+});
+*/
+
+angular.module("todoapp").controller('TodoController',['$scope', '$http', function($scope,$http){
+	$scope.todos = [];
+	var todoId=1;
+
+	$http.get('https://sheltered-shore-4406.herokuapp.com/api/todos/').success(function(todoData){
+		$scope.todos = todoData;
+		console.log($scope.todos);
+	});
+}]);
+
---
+++ public/todoService.js
@@ -0,0 +1,17 @@
+//A service gets data and provides it for controllers
+
+angular.module("todoapp").service("TodoService",function($http,$q)
+{
+	var deferred = $q.defer();//it is a prommis that will be called after alll the other stiff is done
+	$http.get('tododata.json').then(function(data){
+		deferred.resolve(data);
+	});
+
+	//function that returns the defered promise
+	this.getAllTodos = function()
+	{
+		return deferred.promise;
+	}
+})
+
+
