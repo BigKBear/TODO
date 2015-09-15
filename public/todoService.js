26d29fa65d389fc7759ad7e11db5ad211645e131
BigKBear <kylesthill77@gmail.com>
2015-09-15 18:17:34 -0400

    Trying to place code in correct areas

--- public/todoService.js
+++ public/todoService.js
@@ -12,16 +12,4 @@ angular.module("todoapp").service("TodoService",function($http,$q)
 	{
 		return deferred.promise;
 	}
-
-	this.addTodo = function(){
-		$http.post('https://sheltered-shore-4406.herokuapp.com/api/todos/',{name:$scope.addtodo,isDone:false})
-	};
-	
-	this.deleteTodo = function(todoId) {
-		$http.delete( 'https://sheltered-shore-4406.herokuapp.com/api/todos/',{_id:$scope.todoId})
-	};
-
-	this.updateTodo = function(todoId){
-		$http.put('https://sheltered-shore-4406.herokuapp.com/api/todos/',{name:$scope.addtodo,isDone:false})
-	};
 })
