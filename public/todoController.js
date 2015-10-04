449f1f4cf838ff7ef7d1edbf1b2da90bce5980cb
BigKBear <kylesthill77@gmail.com>
2015-10-04 15:58:40 -0400

    TODO: broke code when using a service with promisses

--- public/todoController.js
+++ public/todoController.js
@@ -1,21 +1,48 @@
-angular.module("todoapp").controller('TodoController',['$scope', '$http', function($scope,$http){
+angular.module("todoapp").controller('TodoController',['$scope', '$http', function($scope,todo){
 	//$scope.todos = [];
 	//var todoId=1;
 	//$url = 'https://sheltered-shore-4406.herokuapp.com/api/todos/';
+	$scope.init = function(){
+		$scope.getAll();
+	}
 
+	$scope.getAll = function(){
+		todo.getAllTodos()
+		.then(function(res){
+			//success
+		},function(err){
+			//error
+		})
+	}
+
+	$scope.deletTodo = function(id){
+		task.deletTodo(id)
+		.then(function(res){
+			//success
+			$scope.getAll();
+		},function(err){
+			//error
+		})
+	}
+
+
+	
+	$scope.init();
+
+/*
 	$http.get('https://sheltered-shore-4406.herokuapp.com/api/todos/').success(function(todoData){
 		$scope.todos = todoData;
 	});
-
+*/
 	$scope.addTodo = function(){
 		$http.post($rootScope.endPoint,{name:$scope.addtodo,isDone:false})
 	};
-	
+/*	
 	$scope.deleteTodo = function(todoId) {
 		console.log($scope.todos[todoId]._id);
 		$http.delete($rootScope.endPoint + $scope.todos[todoId]._id,{todoId})
 	};
-
+*/
 	$scope.updateTodo = function(todoId,olddata){
 		console.log($scope);
 		console.log(olddata);
--- public/todoService.js
+++ public/todoService.js
@@ -1,6 +1,42 @@
 //A service gets data and provides it for controllers
 
-angular.module("todoapp").service("TodoService",function task($http, $q, $rootScope) {
-	
+angular.module("todoapp").service("TodoService",function todo($http, $q, $rootScope) {
+	var todo = this;
 
-})
+	todo.getAllTodos = function(){
+		var defer = $q.defer();
+
+		$http.get('https://sheltered-shore-4406.herokuapp.com/api/todos/').success(function(todoData){
+		defer.resolve(todoData);//$scope.todos = todoData;
+	})
+		.error(function(err, status){
+			defer.reject(err);
+		})
+		return defer.promise;
+	}
+	
+	todo.createTodo = function(todo){
+		var defer = $q.defer();
+		$http.post($rootScope.endPoint,{name:$scope.addtodo,isDone:false}).success(function(res){
+			defer.resolve(res);
+		})
+		.error(function(err,status){
+			defer.reject(err);
+		})
+		return defer.promise;
+	}
+
+	todo.deleteTodo = function(id){
+		var defer = $q.defer();
+
+		$http.delete($rootScope.endPoint + $scope.todos[todoId]._id,{todoId}).success(function(res){
+			defer.resolve(res);
+		})
+		.error(function(err, status){
+			defer.reject(err);
+		})
+
+		return defe.promise;
+	}
+	return todo;
+});
